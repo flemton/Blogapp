c2bf23befa918c14cef7c7620f10c93b2c08c621
Osman Alhassan <oalhassan847@gmail.com>
2023-10-25 09:47:58 +0000

    Moving from windows to macOS

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User'
+  attribute :author_id, :integer
+  attribute :text, :string
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :comments, dependent: :destroy
+  has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
+  attribute :text, :string
 end
