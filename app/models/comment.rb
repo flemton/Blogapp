f5db39d6f3edc90013cbc4c2e21d543a6666dadf
Alhassan Osman <oalhassan847@gmail.com>
2023-10-27 08:03:49 +0000

    Add method to update comment counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,10 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   attribute :text, :string
+
+  private
+
+  def update_comment_counter
+    post.update(comments_counter: post.comments.all.length)
+  end
 end
