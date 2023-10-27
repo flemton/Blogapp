1c08ff641e104acb8d22bb636c25a0fc26431164
Alhassan Osman <oalhassan847@gmail.com>
2023-10-27 10:40:44 +0000

    Add validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   has_many :likes
   attribute :bio, :string
 
+  validates :name, presence: true
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+
   def three_recent_posts
     posts.order(created_at: :desc).limit(3)
   end
