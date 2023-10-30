d8883abb19ea7a444b2fb374f8f54c451f6cebaa
Alhassan Osman <oalhassan847@gmail.com>
2023-10-30 07:07:03 +0000

    Add route for displaying a specific post by user

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   get '/users', to: 'users#index'  # For displaying all users
   get '/users/:id', to: 'users#show', as: 'user'  # For displaying a single user
-  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts' # For displaying a user's posts
+  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts' # For displaying a user's postsget '/users/:user_id/posts/:post_id', to: 'posts#show', as: 'user_post'
+  get '/users/:user_id/posts/:post_id', to: 'posts#show', as: 'user_post' # For displaying a specific post by user
 
 end
