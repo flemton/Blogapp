01bfb4bb9dc0f43a15a12b0e8ccd338a78e9ea4b
Alhassan Osman <oalhassan847@gmail.com>
2023-10-30 07:11:13 +0000

    Add action to view a specific post by user

--- app/controllers/posts_contrl_controller.rb
+++
@@ -1,6 +0,0 @@
-class PostsContrlController < ApplicationController
-  def index
-    # For displaying all posts by a given user
-  end
-
-end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,10 @@
 class PostsController < ApplicationController
+  def index
+    # For displaying all posts by a given user
+  end
+
+  def show
+    # For displaying a single post by a user
+  end
+  
 end
