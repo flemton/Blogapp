e9889f8b448f79d37cb6f498fe3ea12348d871da
Alhassan Osman <oalhassan847@gmail.com>
2023-10-31 04:40:41 +0000

    Add test to check text in body for a user

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -33,5 +33,12 @@ RSpec.describe 'Users', type: :request do
       get user_path(user)
       expect(response).to render_template(:show)
     end
+
+    it "includes the correct placeholder text in the response body" do
+      user = User.new(id: 1, name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
+                      posts_counter: 1)
+      get user_path(user)
+      expect(response.body).to include("Here is a list of posts for a given user") # Placeholder text in the view
+    end
   end
 end
