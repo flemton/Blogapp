f1c1494ba2276fbab689d464eb3546daf7751670
Alhassan Osman <oalhassan847@gmail.com>
2023-10-31 04:33:46 +0000

    Fix rubocop offenses

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,26 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET /users/:user_id/posts" do
-    it "returns a successful response for the posts index" do
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /users/:user_id/posts' do
+    it 'returns a successful response for the posts index' do
       user = User.new(id: 1, name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
-      posts_counter: 1)
+                      posts_counter: 1)
       get user_posts_path(user)
       expect(response).to have_http_status(200)
     end
 
-    it "renders the posts index template" do
+    it 'renders the posts index template' do
       user = User.new(id: 1, name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
-      posts_counter: 1)
+                      posts_counter: 1)
       get user_posts_path(user)
       expect(response).to render_template(:index)
     end
   end
 
-  describe "GET /users/:user_id/posts/:id" do
-    it "returns a successful response for a post" do
+  describe 'GET /users/:user_id/posts/:id' do
+    it 'returns a successful response for a post' do
       user = User.new(id: 1, name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
-      posts_counter: 1)
+                      posts_counter: 1)
       post = Post.new(
         id: 1,
         title: 'Sample Title',
@@ -32,9 +32,9 @@ RSpec.describe "Posts", type: :request do
       expect(response).to have_http_status(200)
     end
 
-    it "renders the post show template" do
+    it 'renders the post show template' do
       user = User.new(id: 1, name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
-      posts_counter: 1)
+                      posts_counter: 1)
       post = Post.new(
         id: 1,
         title: 'Sample Title',
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,30 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /users" do
-    it "returns a successful response for the users index" do
+RSpec.describe 'Users', type: :request do
+  describe 'GET /users' do
+    it 'returns a successful response for the users index' do
       get users_path
       expect(response).to have_http_status(200)
     end
 
-    it "renders the users index template" do
+    it 'renders the users index template' do
       get users_path
       expect(response).to render_template(:index)
     end
   end
 
-  describe "GET /users/:id" do
-    it "returns a successful response for a user" do
-      user =  User.new(id: 1, name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
-      posts_counter: 1) # Create a user
-      puts (user_path(user))
+  describe 'GET /users/:id' do
+    it 'returns a successful response for a user' do
+      user = User.new(id: 1, name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
+                      posts_counter: 1) # Create a user
+      puts(user_path(user))
       get user_path(user)
       expect(response).to have_http_status(200)
     end
 
-    it "renders the user show template" do
-      user =  User.new(id: 1, name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
-      posts_counter: 1)
+    it 'renders the user show template' do
+      user = User.new(id: 1, name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
+                      posts_counter: 1)
       get user_path(user)
       expect(response).to render_template(:show)
     end
