24372442b813a5d86080174fb861efbe228d6d9d
Alhassan Osman <oalhassan847@gmail.com>
2023-11-01 04:21:11 +0000

    Add validation for bio on users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :bio, length: { maximum: 255 }
 
   def three_recent_posts
     posts.order(created_at: :desc).limit(3)
