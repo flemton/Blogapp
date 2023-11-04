b47ea86d43ac2af5a3cfe4be195ba8da6ec72478
Alhassan Osman <oalhassan847@gmail.com>
2023-11-04 22:56:47 +0000

    Styled views

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  get '/', to: 'users#index'  # For root
   get '/users', to: 'users#index'  # For displaying all users
   get '/users/:id', to: 'users#show', as: 'user'  # For displaying a single user
   get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts' # For displaying a user's postsget '/users/:user_id/posts/:post_id', to: 'posts#show', as: 'user_post'
