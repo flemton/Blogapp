2fbfc8c9359614701a1b893ec3e95a721e0b6b8a
Alhassan Osman <oalhassan847@gmail.com>
2023-11-10 16:28:49 +0000

    Add rememberable to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_10_161110) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_10_162651) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,6 +58,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_10_161110) do
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.string "encrypted_password"
+    t.datetime "remember_created_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   end
 
