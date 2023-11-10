1abe6e4ac91c54d0675b94488e58079389a0e78b
Alhassan Osman <oalhassan847@gmail.com>
2023-11-10 16:29:47 +0000

    Permit name field in user registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def current_user
     @current_user ||= User.first
   end
   helper_method :current_user
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
