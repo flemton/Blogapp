ea37cb2955e1aaadcb401e698de3891b58197555
Alhassan Osman <oalhassan847@gmail.com>
2023-11-11 12:44:53 +0000

    Update root index syntax

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
 
-  get '/', to: 'users#index'  # For root
   get '/users', to: 'users#index'  # For displaying all users
   get '/users/:id', to: 'users#show', as: 'user'  # For displaying a single user
   get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts' # For displaying a user's postsget '/users/:user_id/posts/:post_id', to: 'posts#show', as: 'user_post'
